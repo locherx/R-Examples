1585458f399cc53469f62615003cda06a7923c4c
locherx <locherx@gmail.com>
2019-02-27 18:07:07 +0100

    Reorganizing code

--- datatable-examples.R
+++ datatable-examples.R
@@ -13,145 +13,149 @@ library(data.table)
 ## data.table(Boston)
 ## as.data.table(Boston)
 data(Boston)
-dat <- data.table(Boston)
-dat
+dtbl1 <- data.table(Boston)
+dtbl1
 
-class(dat)
+class(dtbl1)
 
 ## data.table is also a data.frame
-sapply(dat, is)
+sapply(dtbl1, is)
 
 ## Addressing data --------------------
 ## subset rows from 11 to 20
-dat[11:20]
-dat[11:20, ]
+dtbl1[11:20]
+dtbl1[11:20, ]
 
-## subset rows from 11 to 20 of variable age (result is a vector, not a data.table)
-dat[11:20, age]
+## subset rows from 11 to 20 of variable age (result is a vector, not a dtbl1a.table)
+dtbl1[11:20, age]
 
 ## to get data.table after subsetting, use list() or .() in j argument
-dat[11:20, list(age)]
-dat[11:20, .(age)]
+dtbl1[11:20, list(age)]
+dtbl1[11:20, .(age)]
 
 ## the usual data.frame subsetting style (with columns in j argument)
-dat[11:20, 1]
+dtbl1[11:20, 1]
 
 ## Find all rows where tax variable is equal to 216
-dat[tax == 216]
-dat[tax == 216, ]
+dtbl1[tax == 216]
+dtbl1[tax == 216, ]
 
 ## Find all rows where tax is equal to tax of first row
-dat[tax == tax[1], ]
+dtbl1[tax == tax[1], ]
 
 ## Display values of rad (radius) variable
-dat[, table(rad)]
+dtbl1[, table(rad)]
 
 ## you can also select all columns between some two variables
-dat[, indus:age, with = TRUE]
-dat[, indus:age, with = FALSE]
+dtbl1[, indus:age, with = TRUE]
+dtbl1[, indus:age, with = FALSE]
 
-dat
+dtbl1
 ## Without column "age"
-dat[, -"age", with = TRUE]
-dat[, -"age", with = FALSE]
+dtbl1[, -"age", with = TRUE]
+dtbl1[, -"age", with = FALSE]
 
 ## BUT with negative age
-dat[, .(-age), with = TRUE]
+dtbl1[, .(-age), with = TRUE]
 
 ## j argument is actually a function, so in this case it’s
 ## a function calling variable medv:
-dat[, medv]
+dtbl1[, medv]
 
 ## Below it’s a function which is equal to 5
-dat[, 5]
+dtbl1[, 5]
 
 ## Select several variables (result is a data.table)
-dat[, list(nox, age, black)]
+dtbl1[, list(nox, age, black)]
 
 ## Or equivalently:
-dat[, .(nox, age, black)]
+dtbl1[, .(nox, age, black)]
 
-DT <- data.table(x = rep(c("b","a","c"), each = 3),
-                 y = c(1,3,6), v = 1:9)
-DT
-DT[1]
-DT[-c(5, 1),]
-DT[2, 2]
-DT[, v]
-DT[, .SD, .SDcols = "v"]
-DT[["v"]]
-DT[, 3]
-unlist(DT[, 3])
+dtbl2 <- data.table(x = rep(c("b","a","c"), each = 3),
+                    y = c(1,3,6), v = 1:9)
+str(dtbl2)
+dtbl2
 
-DT[, "v", with = FALSE]
+dtbl2[1]
+dtbl2[-c(5, 1),]
+dtbl2[2, 2]
 
-DT[, c(x, y)]
-DT[, list(x, y)]
-DT[, .(x, y)]
+## Returns a vector
+dtbl2[, v]
+dtbl2[["v"]]
+unlist(dtbl2[, 3])
 
-str(DT)
+dtbl2[, c(x, y)]
 
-## Template of data.table DT
-str(DT[0])
+## Returns a data.table
+dtbl2[, "v", with = FALSE]
+dtbl2[, .SD, .SDcols = "v"] ## identical to line above
+dtbl2[, 3]
+
+dtbl2[, list(x, y)]
+dtbl2[, .(x, y)]
+
+## Template of data.table dtbl2
+str(dtbl2[0])
 
 ## Computing --------------------
 ## Find the range of crim (criminality) variable
-dat[, range(crim)]
+dtbl1[, range(crim)]
 
 ## Add a new variable with :=
-dat[, rad.f := as.factor(paste0("f", rad))]
-dat[, levels(rad.f)]
-str(dat)
+dtbl1[, rad.f := as.factor(paste0("f", rad))]
+dtbl1[, levels(rad.f)]
+str(dtbl1)
 
 ## i.e. we defined a new factor variable(rad.f) in the data table
 ## from the integer variable radius (rad), which describes accessibility to radial highways
 
 ## Add many new variables with `:=`().
 ## If a variable attains only a single value, copy it for each observation
-dat[, `:=`(Var1 = mean(nox), Var2 = sd(age), Var3 = mad(black))]
+dtbl1[, `:=`(Var1 = mean(nox), Var2 = sd(age), Var3 = mad(black))]
 
 ## Compute a more complicated function for groups.
 ## It’s a weighted mean of house prices, with dis
 ## (distances to Boston employment centers) as weights
-dat[, sum(medv*dis)/sum(dis), by = rad.f ]
+dtbl1[, sum(medv*dis)/sum(dis), by = rad.f ]
 
 ## Computing mean of house prices for every level of rad.f
-dat[, mean(medv), by = rad.f]
+dtbl1[, mean(medv), by = rad.f]
 
 ## Computing several variables at once
-dat[, .(meanNox = mean(nox), sdAge = sd(age), madBlack = mad(black))]
+dtbl1[, .(meanNox = mean(nox), sdAge = sd(age), madBlack = mad(black))]
 
 ## Changing a subset of observations --------------------
 ## Let’s create another factor variable crim.f
 ## with 3 levels standing for low, medium and severe crime rates per capita:
 
-dat[          , crim.f := "low"]
-dat[crim >= 1 , crim.f := "medium"]
-dat[crim >= 10, crim.f := "severe"]
-str(dat)
+dtbl1[          , crim.f := "low"]
+dtbl1[crim >= 1 , crim.f := "medium"]
+dtbl1[crim >= 10, crim.f := "severe"]
+str(dtbl1)
 ## crim.f is still a character, contrary to data.frame!
 
-dat[, crim.f := as.factor(crim.f)][]
-str(dat)
+dtbl1[, crim.f := as.factor(crim.f)][]
+str(dtbl1)
 
-table(dat$crim.f)
+table(dtbl1$crim.f)
 
 ## we can also apply functions in j on two or more groups
-dat[, .(mean(medv), sd(medv)), by = .(rad.f, crim.f) ]
+dtbl1[, .(mean(medv), sd(medv)), by = .(rad.f, crim.f) ]
 
 
 ## Aggregating and merging --------------------
 
 ## Aggregating
-dat[, .(meanNox = mean(nox), sdAge = sd(age), madBlack = mad(black)), by = rad.f]
-dat[, .(meanNox = mean(nox), sdAge = sd(age), madBlack = mad(black)), by = rad]
+dtbl1[, .(meanNox = mean(nox), sdAge = sd(age), madBlack = mad(black)), by = rad.f]
+dtbl1[, .(meanNox = mean(nox), sdAge = sd(age), madBlack = mad(black)), by = rad]
 
 ## Aggregating and merging results in original data.table
-dat[, meanNox := mean(nox), by = rad.f]
-dat
+dtbl1[, meanNox := mean(nox), by = rad.f]
+dtbl1
 
 ## cf. with
-dat[, mean(nox), by = rad.f]
+dtbl1[, mean(nox), by = rad.f]
 
 ## Merging
 (x <- data.table(k = 5:0, a = 20:25, zoo = 5:0 ))
@@ -175,87 +179,94 @@ merge(x, y, by.x = "k", by.y = "l", all = TRUE)
 ## Now let’s create a variable of weighted means (mean_w),
 ## and then use it to create a variable for weighted standard deviation (std_w).
 
-dat[,  `:=`(meanW = meanW <- sum(medv*dis)/sum(dis),
-            stdW = sqrt(sum(dis*(medv - meanW)^2 )/sum(dis))),
-    by = rad.f][]
+dtbl1[,  `:=`(meanW = meanW <- sum(medv*dis)/sum(dis),
+              stdW = sqrt(sum(dis*(medv - meanW)^2 )/sum(dis))),
+      by = rad.f][]
 
 ## To use some variables with long names, specify them in .SDcols and use .SD instead:
-dat[, `:=`(x = sum(.SD[[1]]^2) / sum(.SD[[1]]),
-           y = sum(.SD[[2]]^2) / sum(.SD[[2]]) ),
-        by = rad.f,
-    .SDcols = c("medv", "age")][]
+dtbl1[, `:=`(x = sum(.SD[[1]]^2) / sum(.SD[[1]]),
+             y = sum(.SD[[2]]^2) / sum(.SD[[2]]) ),
+      by = rad.f,
+      .SDcols = c("medv", "age")][]
 
 ## Multiple expressions in j could be handled with { }
 par(mfrow = c(1,2))
-dat[, {hist(log(crim), col = "royalblue3")
-       plot(rm, medv, pch = 16)
-       grid()}]
+dtbl1[, {hist(log(crim), col = "royalblue3")
+         plot(rm, medv, pch = 16)
+         grid()}]
+
+##???
+dtbl1[, lapply(.SD, hist), .SDcols = c("crim", "age")]
+dtbl1[, lapply(seq(.SD), function(i, x) hist(x[[i]], .SD)), .SDcols = c("crim", "age")]
 
 ## New data.table with dynamically created variables can be created by
-dat[, {list(meanW = meanW <- sum(medv*dis)/sum(dis),
-            stdW = sqrt( sum(dis*(medv - meanW)^2 )/sum(dis))
-            )},
+dtbl1[, {list(meanW = meanW <- sum(medv*dis)/sum(dis),
+              stdW = sqrt( sum(dis*(medv - meanW)^2 )/sum(dis))
+              )},
       by = rad.f]
 
 ## Chaining --------------------
-dat[, crim.f := "low"] [crim >= 1, crim.f := "medium"]
-dat[ crim >= 10, crim.f := "severe"][, crim.f := as.factor(crim.f)]
-levels(dat$crim.f)
+dtbl1[, crim.f := "low"] [crim >= 1, crim.f := "medium"]
+dtbl1[ crim >= 10, crim.f := "severe"][, crim.f := as.factor(crim.f)]
+levels(dtbl1$crim.f)
 
 ## Equivalent chaining in one command
-dat[ , crim.f := "low"] [
-  crim >= 1, crim.f := "medium"] [
-    crim >= 10, crim.f := "severe"][,
-      , crim.f := as.factor(crim.f)]
+dtbl1[ , crim.f := "low"] [
+                    crim >= 1, crim.f := "medium"] [
+                                          crim >= 10, crim.f := "severe"][,
+                                                               , crim.f := as.factor(crim.f)]
 
 ## Renaming column names --------------------
-names(dat)
-setnames(dat, c("rm", "zn"), c("rooms_average", "proportion_zoned") )
-names(dat)
+names(dtbl1)
+setnames(dtbl1, c("rm", "zn"), c("rooms_average", "proportion_zoned") )
+names(dtbl1)
 
 ## Sorting columns
-setcolorder(dat, rev(names(dat)))
-names(dat)
+setcolorder(dtbl1, rev(names(dtbl1)))
+names(dtbl1)
 
 ## Sorting rows --------------------
 ## set the key. Order is always ascending
-setkey(dat, rad.f, crim.f)
+setkey(dtbl1, rad.f, crim.f)
 
 ## use binary search (fast, O(log(n) )
-dat[.("f7", "low"), .(rad.f, crim.f, tax)]
+dtbl1[.("f7", "low"), .(rad.f, crim.f, tax)]
 
-dat[, .(crim, tax)]
-setorder(dat, crim, tax)
-dat[, .(crim, tax)]
-setorder(dat, -crim, tax) ## Descending order for crim!
-dat[, .(crim, tax)]
+dtbl1[, .(crim, tax)]
+setorder(dtbl1, crim, tax)
+dtbl1[, .(crim, tax)]
+setorder(dtbl1, -crim, tax) ## Descending order for crim!
+dtbl1[, .(crim, tax)]
 
 ## DO NOT use vector scan (slow, O(n) )
-dat[rad.f =="f7" & crim.f == "low"]
+dtbl1[rad.f =="f7" & crim.f == "low"]
 
 ## Avoid using data.frame’s vector scan inside data.table:
-dat[dat$rad.f == "f7" & dat$crim.f == "low", ]
+dtbl1[dtbl1$rad.f == "f7" & dtbl1$crim.f == "low", ]
 
 
 ## Special functions --------------------
 ## .N function count the number observations in a group:
-dat[, .N, by = .(rad.f, crim.f)]
-dat[, .N]
+dtbl1[, .N, by = .(rad.f, crim.f)]
+dtbl1[, .N]
 
 ## Another useful function is .SD which contains values
 ## of all variables except the one used for grouping
-dat[, .SD[1], by =  crim.f ]
-dat[, lapply(.SD, mean), by =  crim.f, .SDcols = tax:age]
-dat[, lapply(.SD, mean), by =  crim.f, .SDcols = c("tax", "age")]
+dtbl1[, .SD[1], by =  crim.f]
+dtbl1[, lapply(.SD, mean), by =  crim.f, .SDcols = tax:age]
+dtbl1[, lapply(.SD, mean), by =  crim.f, .SDcols = c("tax", "age")]
 
 ## Group counter .GRP
-dat[, .(.I, .GRP, .N), by =  crim.f]
+dtbl1[, .(.I, .GRP, .N), by =  crim.f]
 
 ## Runtime Length ID
 dtab <- data.table(bit = sample(0:1, 20, replace = TRUE),
                    L = sample(LETTERS[1:3], 20, replace = TRUE))
 dtab[, .SD]
 rleid(dtab$bit)
+dtab[, bit, by = rleid(bit)]
+dtab[, bit, by = rleid(L)]
+setkey(dtab, bit)
 dtab[, bit, by = rleid(bit)]
 
 ## data.table within functions --------------------
@@ -266,13 +277,10 @@ testScope0 <-
     }
 
 dtab1 <- testScope0(dtab)
-dtab1
-
-dtab
-
 identical(dtab, dtab1)
 ## dtab AND dtab1 are identical!!!
 ## -> data.table are passed by reference (exception!!)
+dtab1
 rm(dtab1)
 
 testScope1 <-
@@ -309,8 +317,8 @@ identical(dtab, dtab2)
 ## Adressing variables dynamically within a function ----------
 testDynProg1 <-
     function(dtab, nam){
-       return(dtab[, nam, with = FALSE])
-   }
+        return(dtab[, nam, with = FALSE])
+    }
 
 testDynProg1(dtab, "bit")  ## ok
 testDynProg1(dtab, .(bit)) ## error
@@ -318,7 +326,7 @@ testDynProg1(dtab, .(bit)) ## error
 testDynProg2 <-
     function(dtab, expr){
         e <- substitute(expr)
-       return(dtab[, eval(e)])
+        return(dtab[, eval(e)])
     }
 
 testDynProg2(dtab, "bit")  ## error
